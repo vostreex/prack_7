4a847a6d4c95c16c11cbb40888dfdaa238c11aa4
vostreex <vostan97@gmail.com>
2025-10-22 20:15:38 +0300

    new configuration for navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:prack_7/features/notes/state/notes_container.dart';
+import 'package:go_router/go_router.dart';
+import 'package:prack_7/app_router.dart';
 import 'shared/theme/app_theme.dart';
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
+  final GoRouter _router = AppRouter().router;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notes App',
+    return MaterialApp.router(
+      routerConfig: _router,
       theme: AppTheme.lightTheme,
-      home: const NotesContainer(),
     );
   }
 }
